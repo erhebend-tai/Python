3d4ff8c355efbd1fd314b1e9629a9fc5ff79d912
Barton Milnor Mallory <205405648+erhebend-tai@users.noreply.github.com>
2025-10-08 04:57:32 -0500

    Fixed Typo

--- other/bankers_algorithm.py
+++ other/bankers_algorithm.py
@@ -7,7 +7,7 @@
 """
 The Banker's algorithm is a resource allocation and deadlock avoidance algorithm
 developed by Edsger Dijkstra that tests for safety by simulating the allocation of
-predetermined maximum possible amounts of all resources, and then makes a "s-state"
+predetermined maximum possible amounts of all resources, and then makes an "s-state"
 check to test for possible deadlock conditions for all other pending activities,
 before deciding whether allocation should be allowed to continue.
 
